e3f33f4f2d8fe708c176da1256bab70349a15382
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2026-06-28 19:11:31 +0330

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@devmahmoudi/ui";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center">Hello</h1>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
